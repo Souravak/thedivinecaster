1229bc68a6c1bd67f75df54efd8339b8998fd87b
souravak <souravak211@gmail.com>
2024-01-06 13:25:43 +0530

    query update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
+import About from './components/About/About';
 import './App.css';
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
+      <About/>
     </div>
   );
 }
---
+++ src/components/About/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+  return (
+    <div className="about">
+        <div className="title">About</div>
+    </div>
+  )
+}
+
+export default About
